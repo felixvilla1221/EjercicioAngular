d0982c1d7009d9ecf294072d36150d711c61e358
felixvilla1221 <felixvilla51@gmail.com>
2022-10-05 20:14:07 -0500

    concepto de typescripts

---
+++ src/app/recap.ts
@@ -0,0 +1,9 @@
+const username = 'Felix villamizar';
+const username2: string ='Felix Villamizar';
+const username3: string | number ='Felix villamizar';
+
+const sum = (a:number , b:number) => {
+    return a + b;
+}
+
+sum(1 , 4);
