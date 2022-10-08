f02cd92bf8f35bda11bef7ad86f4dc70d3487158
felixvilla1221 <felixvilla51@gmail.com>
2022-10-07 20:36:27 -0500

    cambios de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-prueba';
-}
+    sum = (a:number , b:number) => {
+    return a + b;
+  }
+ 
+  }
+
+ 
+
