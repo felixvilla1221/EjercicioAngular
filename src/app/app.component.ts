fae63f1593a9cc5464340c42410c533563e7aff4
felixvilla1221 <felixvilla51@gmail.com>
2022-10-10 19:02:55 -0500

    interpolacion en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-prueba';
-    sum = (a:number , b:number) => {
-    return a + b;
-  }
+  title = 'PRUEBA';
+  name  = 'Felix';
+  age   = 40; 
+  img   = 'https://www.w3schools.com/howto/img_avatar.png';
  
   }
 
