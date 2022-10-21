7e0d88d9d2aa77305981c57c7507f1505d94a5ab
felixvilla1221 <felixvilla51@gmail.com>
2022-10-20 19:58:07 -0500

    update logica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PRUEBA';
+  username = 'felixvillamizar';
+  username2:string ='Juan Camilo';
+  username3:string | number = 84090351;
+  email: string = 'felixvilla51@gmail.com';
 
-  Persona  = {
-  name:'',
-  age:40,
-  phone:3223439827,
-    
-  } 
-
-
-
-  
+  persona = {
+  name:'Magdalena Martinez',
+  age:54,  
   }
+
+  btnDisable = true;
+
+}
+  
+  
 
  
 
